8427c6cc42901dd3f5e99ec245fb05a4cc3529cb
Dubass83 <makssych@gmail.com>
2023-11-23 21:07:08 +0200

    feat: add util package

---
+++ util/random.go
@@ -0,0 +1,50 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "qazwsxedcrfvtgbyhnujmikolp"
+
+func init() {
+	rand.NewSource(time.Now().UnixNano())
+}
+
+// RandomInt return random int64 between min and max values
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString return random string of given length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+// RandomOwner generate a random Owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomMoney generate a random amount of Money
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+// RandomCurrency generate a random currency code
+func RandomCurrency() string {
+	cur := []string{
+		"USD",
+		"EUR",
+		"UAH",
+	}
+	return cur[rand.Intn(len(cur))]
+}
