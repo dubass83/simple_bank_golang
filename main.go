e0491f97aeab4316e39d36ece2b5e0194c537197
Dubass83 <makssych@gmail.com>
2023-12-08 20:48:03 +0200

    feat: add run server command from main func

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+
+	"github.com/dubass83/simplebank/api"
+	db "github.com/dubass83/simplebank/db/sqlc"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://postgres:postgres@localhost:5432/simple_bank?sslmode=disable"
+	addressString = "0.0.0.0:8080"
+)
+
 func main() {
 
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("can not connect to db", err)
+	}
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(addressString)
+	if err != nil {
+		log.Fatal("can not start server", err)
+	}
 }
