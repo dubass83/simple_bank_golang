59cf6439453dfc317a62e43edb590520ad0cc150
Dubass83 <makssych@gmail.com>
2024-01-21 18:18:14 +0200

    feat: add custom Pg error

---
+++ db/sqlc/error.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+const (
+	ForeignKeyViolation = "23503"
+	UniqueViolation     = "23505"
+)
+
+var ErrRecordNotFound = pgx.ErrNoRows
+
+var ErrUniqueViolation = &pgconn.PgError{
+	Code: UniqueViolation,
+}
