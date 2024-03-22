150f7d8ae787b3962712565f4d47f7361d3e386d
Dubass83 <makssych@gmail.com>
2024-03-22 21:45:32 +0200

    feat: complete create httpLoger

--- gapi/logger.go
+++ gapi/logger.go
@@ -42,24 +42,44 @@ func GrpcLogger(
 	return result, err
 }
 
+type ResponseRecorder struct {
+	http.ResponseWriter
+	StatusCode int
+	Body       []byte
+}
+
+func (rec *ResponseRecorder) WriteHeader(statusCode int) {
+	rec.StatusCode = statusCode
+	rec.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (rec *ResponseRecorder) Write(body []byte) (int, error) {
+	rec.Body = body
+	return rec.ResponseWriter.Write(body)
+}
+
 func HttpLogger(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
 		startTime := time.Now()
-		handler.ServeHTTP(res, req)
+		rec := &ResponseRecorder{
+			ResponseWriter: res,
+			StatusCode:     http.StatusOK,
+		}
+		handler.ServeHTTP(rec, req)
 		duration := time.Since(startTime)
 
 		logger := log.Info()
-		// if err != nil {
-		// 	logger = log.Error().Err(err)
-		// // }
+		if rec.StatusCode != http.StatusOK {
+			logger = log.Error().Bytes("body", rec.Body)
+		}
 
 		logger.Str("protocol", "http").
 			Str("method", req.Method).
 			Str("method_path", req.RequestURI).
-			// Int("status_code", int(statusCode)).
-			// Str("status_text", statusCode.String()).
+			Int("status_code", rec.StatusCode).
+			Str("status_text", http.StatusText(rec.StatusCode)).
 			Dur("duration", duration).
-			Msg("receive GRPC request")
+			Msg("receive HTTP request")
 
 	})
 }
