ca0987aee5fc0dfa5faa95ac9dffcbc3882a41db
Dubass83 <makssych@gmail.com>
2024-06-06 10:11:55 +0300

    chore: complete writing tests for account delete function

--- gapi/rpc_delete_account_test.go
+++ gapi/rpc_delete_account_test.go
@@ -2,6 +2,8 @@ package gapi
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"testing"
 	"time"
 
@@ -12,6 +14,8 @@ import (
 	"github.com/dubass83/simplebank/util"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func TestDeleteAccount(t *testing.T) {
@@ -51,6 +55,144 @@ func TestDeleteAccount(t *testing.T) {
 			checkResponce: func(t *testing.T, res *pb.DeleteAccountResponse, err error) {
 				require.NotNil(t, res)
 				require.NoError(t, err)
+				require.Equal(t, fmt.Sprintf("%d", account.ID), res.GetAccountWasDeleted())
+			},
+		}, {
+			name: "InternalErrorFromGetAccount",
+			req:  &pb.DeleteAccountRequest{Id: account.ID},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(db.Account{}, sql.ErrConnDone)
+
+				store.EXPECT().
+					DeleteAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(0)
+			},
+			buildContext: func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return BuildContext(t, tokenMaker, user.Username, time.Minute)
+			},
+			checkResponce: func(t *testing.T, res *pb.DeleteAccountResponse, err error) {
+				require.Nil(t, res)
+				require.Error(t, err)
+				status, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.Internal, status.Code())
+			},
+		}, {
+			name: "InternalErrorFromDeleteAccount",
+			req:  &pb.DeleteAccountRequest{Id: account.ID},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(account, nil)
+
+				store.EXPECT().
+					DeleteAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(sql.ErrConnDone)
+			},
+			buildContext: func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return BuildContext(t, tokenMaker, user.Username, time.Minute)
+			},
+			checkResponce: func(t *testing.T, res *pb.DeleteAccountResponse, err error) {
+				require.Nil(t, res)
+				require.Error(t, err)
+				status, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.Internal, status.Code())
+			},
+		}, {
+			name: "AccountNotFoundError",
+			req:  &pb.DeleteAccountRequest{Id: account.ID},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(db.Account{}, sql.ErrNoRows)
+
+				store.EXPECT().
+					DeleteAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(0)
+			},
+			buildContext: func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return BuildContext(t, tokenMaker, user.Username, time.Minute)
+			},
+			checkResponce: func(t *testing.T, res *pb.DeleteAccountResponse, err error) {
+				require.Nil(t, res)
+				require.Error(t, err)
+				status, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.NotFound, status.Code())
+			},
+		}, {
+			name: "DeleteFromAnotherAccountError",
+			req:  &pb.DeleteAccountRequest{Id: account.ID},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(account, nil)
+
+				store.EXPECT().
+					DeleteAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(0)
+			},
+			buildContext: func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return BuildContext(t, tokenMaker, "dubass83", time.Minute)
+			},
+			checkResponce: func(t *testing.T, res *pb.DeleteAccountResponse, err error) {
+				require.Nil(t, res)
+				require.Error(t, err)
+				status, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.Unauthenticated, status.Code())
+			},
+		}, {
+			name: "UnauthenticatedError",
+			req:  &pb.DeleteAccountRequest{Id: account.ID},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(0)
+
+				store.EXPECT().
+					DeleteAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(0)
+			},
+			buildContext: func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return BuildContext(t, tokenMaker, user.Username, -time.Minute)
+			},
+			checkResponce: func(t *testing.T, res *pb.DeleteAccountResponse, err error) {
+				require.Nil(t, res)
+				require.Error(t, err)
+				status, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.Unauthenticated, status.Code())
+			},
+		}, {
+			name: "InvalidArgumentError",
+			req:  &pb.DeleteAccountRequest{Id: -1},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Any()).
+					Times(0)
+
+				store.EXPECT().
+					DeleteAccount(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			buildContext: func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return BuildContext(t, tokenMaker, user.Username, time.Minute)
+			},
+			checkResponce: func(t *testing.T, res *pb.DeleteAccountResponse, err error) {
+				require.Nil(t, res)
+				require.Error(t, err)
+				status, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.InvalidArgument, status.Code())
 			},
 		},
 	}
